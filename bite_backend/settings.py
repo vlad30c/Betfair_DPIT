67ad88ba1dbcccca27a91ee6eb17a906d545e874
Rebecca <maruscarebecca@gmail.com>
2025-09-17 17:28:33 +0300

    debug false

--- bite_backend/settings.py
+++ bite_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hdu@&mk+ygi++4nh!6308t^5+44#vjg7!^ubrlgz$ng@3%%%-z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
